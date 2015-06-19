bee8ae93b643648ef8a7a49bb82379e58c70b8ea
dcbowman <dcbowman@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-06-19 19:15:44 +0000

    Further test script development
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rnomads@35 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/rNOMADS/rnomads_test/test_grids.R
+++ trunk/rNOMADS/rnomads_test/test_grids.R
@@ -2,4 +2,13 @@
 
 library(rNOMADS)
 
+model.urls <- GetDODSDates("gfs_0p50")
+latest.model <- tail(model.urls$url, 1)
+model.runs <- GetDODSModelRuns(latest.model)
+latest.model.run <- tail(model.runs$model.run, 1)
+
+time <- c(0,0) #Analysis model
+lon <- c(0, 719) #All 720 longitude points
+lat <- c(0, 360) #All 361 latitude points
+
 
