dc8c6769ec870852cf64e39a449c889d722bf195
dcbowman <dcbowman@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-12-30 21:37:44 +0000

    Updated grib vignette to work correctly with new ReadGrib output structure
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rnomads@65 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/rNOMADS/rNOMADS/R/onLoad.R
+++
@@ -1,5 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-   cat(paste(c("Welcome to rNOMADS: Near real time oceanic and atmospheric data at your fingertips.\n",
-   "Questions? Send a message to rnomads-user@lists.r-forge.r-project.org\n",
-   "Using rNOMADS as a data source for a publication?  Please cite it!\n  I'm an early career researcher and every citation matters.\n")))
-}
